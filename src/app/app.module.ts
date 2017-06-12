35a8c13f66bc831ae1e071fbaa1082a5b170ec77
alexb31 <123azertyu>
2017-06-13 00:43:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDdtdIt39ArvP8vuboPDkCFb36FqEYKhIQ",
+    authDomain: "what-i-do-8ac8a.firebaseapp.com",
+    databaseURL: "https://what-i-do-8ac8a.firebaseio.com",
+    projectId: "what-i-do-8ac8a",
+    storageBucket: "what-i-do-8ac8a.appspot.com"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
